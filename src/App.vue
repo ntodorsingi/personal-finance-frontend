3ae27e062219fad79888d2b406660794e62bd4fb
ntodorsingi <nikola.todorovic.22@singimail.rs>
2024-08-20 11:42:58 +0200

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <Navbar />
+    <router-view />
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Navbar from './components/Navbar.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Navbar
+  }
+});
+</script>
 
 <style>
 #app {
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,36 @@
 <template>
-    <div>
-
-    </div>
-</template>
-
-<script setup>
-
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+    <nav>
+      <router-link to="/" class="nav-link">Home</router-link>
+      <router-link to="/dashboard" class="nav-link">Dashboard</router-link>
+      <router-link to="/account/1" class="nav-link">Account Details</router-link> <!-- Primer sa ID-om računa -->
+      <router-link to="/login" class="nav-link">Login</router-link>
+      <router-link to="/register" class="nav-link">Register</router-link>
+    </nav>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  
+  export default defineComponent({
+    name: 'Navbar'
+  });
+  </script>
+  
+  <style scoped>
+  nav {
+    background-color: #333;
+    padding: 10px;
+  }
+  
+  .nav-link {
+    color: white;
+    text-decoration: none;
+    margin: 0 15px;
+    font-weight: bold;
+  }
+  
+  .nav-link:hover {
+    text-decoration: underline;
+  }
+  </style>
+  
