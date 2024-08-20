4b9a133e3ff1713080e79ca0071251ce4f29305b
ntodorsingi <nikola.todorovic.22@singimail.rs>
2024-08-20 10:38:23 +0200

    pocetak

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <Navbar />
+      <h1>Dashboard</h1>
+      <AccountSummary />
+      <Charts />
+      <TransactionList />
+      <Pagination />
+    </div>
+  </template>
+  
+  <script>
+  import Navbar from '@/components/Navbar.vue';
+  import AccountSummary from '@/components/AccountSummary.vue';
+  import Charts from '@/components/Charts.vue';
+  import TransactionForm from '@/components/TransactionForm.vue';
+  import Pagination from '@/components/Pagination.vue';
+  
+  export default {
+    components: {
+      Navbar,
+      AccountSummary,
+      Charts,
+      TransactionForm,
+      Pagination,
+    },
+  };
+  </script>
+  
